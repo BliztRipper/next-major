941db29b044a2371802c4f109d8242adb1920aa9
Arnon Tawong <arnontawong@Arnons-MacBook-ProGodMode.local>
2018-07-09 13:55:28 +0700

    setting component data

--- components/BottomNavBar.jsx
+++ components/BottomNavBar.jsx
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import styled from 'styled-components';
 
 class BottomNavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataObj: [],
+      isLoading: true,
+      error: null,
+    }
+  }
+  componentDidMount(){
+    fetch(`https://api.hackerwebapp.com/news`)
+    .then(response => response.json())
+    .then(data => this.setState({dataObj:data, isLoading: false}))
+    .catch(error => this.setState({ error, isLoading: false }))
+  }
   render() {
+    const {dataObj, isLoading, error} = this.state;
+    if (error) {
+      return <p>{error.message}</p>;
+    }
+
+    if (isLoading) {
+      return <p>Loading...</p>;
+    }
+    {console.log(dataObj)}
     return (
       <Tabs>
         <TabPanel>
           <h2>Any content 1</h2>
+          {dataObj.map((item,i) =>
+          <div key={i}>
+            <h1> {item.title}</h1>
+            <h4> {item.points}</h4>
+          </div>
+        )}
         </TabPanel>
         <TabPanel>
           <h2>Any content 2</h2>
--- pages/index.jsx
+++ pages/index.jsx
@@ -8,11 +8,13 @@ export default class extends Component {
   static async getInitialProps() {
     const req = await fetch(`https://api.hackerwebapp.com/news`)
     const stories = await req.json()
-    return { stories }
+    return {stories}
   }
+  
   render() {
     return(
       <div>
+        {/* {console.log(this.props.stories)} */}
         <Head>
           <title>True Major Cineplex</title>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
