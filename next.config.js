206936ef6e63a7f971211c546d320d0029842ff1
Arnon Tawong <arnontawong@Arnons-MacBook-ProGodMode.local>
2018-07-26 18:15:32 +0700

    remover service worker

--- next.config.js
+++ next.config.js
@@ -1,28 +1,5 @@
-const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
 const withSass = require('@zeit/next-sass')
 const withImages = require('next-images')
 const withOffline = require('next-offline')
 
-module.exports = withSass(withImages(withOffline(
-// {workboxOpts: {
-//   runtimeCaching: [
-//     {
-//       urlPattern: /.png$/,
-//       handler: 'cacheFirst'
-//     },
-//     {
-//       urlPattern: /api/,
-//       handler: 'networkFirst',
-//       options: {
-//         cacheableResponse: {
-//           statuses: [0, 200],
-//           headers: {
-//             'x-test': 'true'
-//             }
-//           }
-//         }
-//       }
-//     ]
-//   }
-// }
-)))
+module.exports = withSass(withImages())
