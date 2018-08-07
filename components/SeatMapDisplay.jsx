70f12b2df3ae2c7625234b6aee7da97a7e3f6c68
korakot.sup <korakot.sup@eBiz-admins-MacBook-Pro.local>
2018-08-07 16:20:29 +0700

    [seatMap] selected seats.

--- components/SeatMapDisplay.jsx
+++ components/SeatMapDisplay.jsx
@@ -1,42 +1,83 @@
-import { PureComponent } from 'react'
+import { PureComponent, Fragment } from 'react'
 import './SeatMapDisplay.scss'
 
 class SeatMapDisplay extends PureComponent {
-  render () {
-    let areas = this.props.areaData
-    let listItems = (area) => {
-      let arr = []
-      arr = area.Rows.map(row => {
-        if (row.PhysicalName !== null) {
-          return (
-            <div className="seatMapDisplay__row">
-              <div className="seatMapDisplay__cell seatMapDisplay__cell-title" key={area.AreaCategoryCode + row.PhysicalName }>{row.PhysicalName}</div>
-              {
-                row.Seats.map((aSeat, index) => {
-                  console.log(aSeat, 'aSeat index')
-                  return (
-                    <div className="seatMapDisplay__cell" key={area.AreaCategoryCode + row.PhysicalName + index}>{(index + 1)}</div>
-                  )
-                })
-              }
-            </div>
-          )
-        }
-      });
-      return arr
-    }       
-    let listGroups = areas.map((area) => {
-      return (
-        <div className={ 'seatMapDisplay__group-' + area.Description.replace(/ /g,'') } key={area.AreaCategoryCode}>
-          <div className="seatMapDisplay__title">{area.Description}</div>
-          {listItems(area)}
-        </div>
-      );
+  constructor(props) {
+    super(props);
+      this.state = {
+        areas: this.props.areaData,
+        seatsSelected: [],
+        renderSeats: null
+      }
+  }
+  handleSelectSeats (aSeat, area, row) {
+    if (aSeat.Status === 99) {
+      aSeat.Status = 0
+    } else if (aSeat.Status === 0) {
+      aSeat.Status = 99
+    }
+    this.state.seatsSelected.push({
+      ...aSeat,
+      AreaCategoryCode: area.AreaCategoryCode,
+      rowPhysicalName: row.PhysicalName
     })
-    console.log(listGroups) 
+    this.setState({
+      renderSeats: this.listGroups(this.state.areas)
+    })
+  }
+  listItems (area) {
+    let arr = []
+    arr = area.Rows.map((row, rowIndex) => {
+      if (row.PhysicalName !== null) {
+        let seatMapCell = row.Seats.map((aSeat, aSeatIndex) => {
+          let classNameCell = 'seatMapDisplay__cell'
+          if (aSeat.Status !== 0) {
+            if (aSeat.Status === 99) {
+              classNameCell = classNameCell + ' ' + 'selected'
+            } else {
+              classNameCell = classNameCell + ' ' + 'notAllowed'
+            }
+          }
+          return (
+            <div className={classNameCell} key={area.AreaCategoryCode + row.PhysicalName + aSeatIndex} onClick={this.handleSelectSeats.bind(this, aSeat, area, row)} >{(aSeat.Id)}</div>
+          )
+        })
+        return (
+          <Fragment key={'Fragment' + row.PhysicalName + rowIndex}>
+            <div className="seatMapDisplay__cell seatMapDisplay__cell-title" key={area.AreaCategoryCode + row.PhysicalName }>{row.PhysicalName}</div>
+            {seatMapCell}
+          </Fragment>
+        )
+      }
+    })
+    return arr
+  }
+  listGroups (areas) {
+    return (
+      areas.map((area) => {
+        let classNameGroupSpecific = area.AreaCategoryCode === '0000000008' ? 'isPremium' : area.AreaCategoryCode === '0000000016' ? 'isPrivilege' : ''
+        return (
+          <div className={ 'seatMapDisplay__group ' + classNameGroupSpecific } key={area.AreaCategoryCode}>
+            <div className="seatMapDisplay__title" key={'title' + area.AreaCategoryCode + area.Description }>{area.Description}</div>
+            <div className="seatMapDisplay__row" key={'row' + area.AreaCategoryCode + area.Description }>
+              {this.listItems(area)}
+            </div>
+          </div>
+        );
+      })
+    )
+  }
+  componentWillMount () {
+    this.setState({ 
+      renderSeats: this.listGroups(this.state.areas) 
+    })
+  }
+  render () {
+    const {renderSeats} = this.state
+    if (!renderSeats) return false
     return (
       <div className="seatMapDisplay">
-        {listGroups}
+        {renderSeats}
       </div>
     )
   }
