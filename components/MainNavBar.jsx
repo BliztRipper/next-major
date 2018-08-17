e16aebdbfdb8ce0b408d0f77a96f05e4ee75c197
korakot.sup <korakot.sup@korakotsup-mbp.local>
2018-08-17 11:12:48 +0700

    Fixed props id error on server render.

--- components/MainNavBar.jsx
+++ components/MainNavBar.jsx
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import { Tab, Tabs, TabList, TabPanel, resetIdCounter } from 'react-tabs';
 import HighlightCarousel from '../components/HighlightCarousel'
 import MainCinemaListing from '../components/MainCinemaListing'
 import Link from 'next/link'
@@ -7,38 +7,39 @@ import Link from 'next/link'
 
 class MainNavBar extends PureComponent {
   render() {
+    resetIdCounter()
     return (
       <div className="indexTab">
         <Tabs>
-        <TabPanel>
-          <Link prefetch href="/AllMovie">
-            <a className="allmovie-btn">ดูภาพยนต์ทั้งหมด</a>
-          </Link>
-          <HighlightCarousel/>
-        </TabPanel>
-        <TabPanel>
-          <MainCinemaListing/>
-        </TabPanel>
-        <TabPanel>
-          <h2>Any content 3</h2>
-        </TabPanel>
-        <TabList>
-          <div className="react-tabs__tabs-container">
-            <Tab>
-              <div className="sprite-tab-menu1"></div>
-              <span className="tab-menu-title">ภาพยนต์</span>
-            </Tab>
-            <Tab>
-              <div className="sprite-tab-menu2"></div>
-              <span className="tab-menu-title">โรงภาพยนต์</span>
-            </Tab>
-            <Tab>
-            <div className="sprite-tab-menu3"></div>
-            <span className="tab-menu-title">ตั๋วหนัง</span>
-            </Tab>
-          </div>
-        </TabList>
-      </Tabs>
+          <TabPanel>
+            <Link prefetch href="/AllMovie">
+              <a className="allmovie-btn">ดูภาพยนต์ทั้งหมด</a>
+            </Link>
+            <HighlightCarousel/>
+          </TabPanel>
+          <TabPanel>
+            <MainCinemaListing/>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 3</h2>
+          </TabPanel>
+          <TabList>
+            <div className="react-tabs__tabs-container">
+              <Tab>
+                <div className="sprite-tab-menu1"></div>
+                <span className="tab-menu-title">ภาพยนต์</span>
+              </Tab>
+              <Tab>
+                <div className="sprite-tab-menu2"></div>
+                <span className="tab-menu-title">โรงภาพยนต์</span>
+              </Tab>
+              <Tab>
+              <div className="sprite-tab-menu3"></div>
+              <span className="tab-menu-title">ตั๋วหนัง</span>
+              </Tab>
+            </div>
+          </TabList>
+        </Tabs>
       </div>
     );
   }
