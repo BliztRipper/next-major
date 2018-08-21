51fd2bdc6d07ac9a9f537448ec8c5bd00d02030f
korakot.sup <korakot.sup@korakotsup-mbp.local>
2018-08-21 14:06:31 +0700

    Seperate ticket component from cashier page.

--- pages/Cashier.jsx
+++ pages/Cashier.jsx
@@ -1,10 +1,9 @@
-import React, { PureComponent, Fragment } from 'react';
+import { PureComponent } from 'react';
 import Layout from '../components/Layout'
+import Ticket from '../components/Ticket'
 import loading from '../static/loading.gif'
-import '../styles/cashier.scss'
-import QRCode from 'qrcode.react'
 
-class CinemaMovieInfo extends PureComponent {
+class Cashier extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
@@ -17,6 +16,7 @@ class CinemaMovieInfo extends PureComponent {
         currency: 'THB',
         payload: {}
       },
+      dataToTicket: '',
       apiOtpHeader: {
         'Accept': 'application/json',
         'X-API-Key': '085c43145ffc4727a483bc78a7dc4aae',
@@ -37,7 +37,7 @@ class CinemaMovieInfo extends PureComponent {
       BookingPriceDisplay: '',
       BookingUserSessionId: '',
       BookingUserPhoneNumber: '',
-      success: false,
+      success: false, 
       VistaBookingId:'',
       VistaBookingNumber:''
     }
@@ -91,8 +91,7 @@ class CinemaMovieInfo extends PureComponent {
         BookingPrice: sessionStorage.getItem('BookingPrice'),
         BookingPriceDisplay: sessionStorage.getItem('BookingPriceDisplay'),
         BookingUserSessionId: sessionStorage.getItem('BookingUserSessionId'),
-        BookingUserPhoneNumber: sessionStorage.getItem('BookingUserPhoneNumber'),
-        isLoading: false
+        BookingUserPhoneNumber: sessionStorage.getItem('BookingUserPhoneNumber')
       },
         () => {
           let filterPattern = 'Booking'
@@ -100,88 +99,26 @@ class CinemaMovieInfo extends PureComponent {
           filtered.forEach(key => { this.state.dataToPayment.payload[key] = this.state[key] })
           this.state.dataToPayment.third_party_tx_id = this.state.BookingUserSessionId
           this.state.dataToPayment.amount_satang = this.state.BookingPrice
+          this.setState({
+            dataToTicket: this.state.dataToPayment.payload,
+            isLoading: false
+          })
         })
     } catch (error) {
       error => this.setState({ error, isLoading: false })
     }
   }
   render() {
-    const {isLoading, error,success, postingTicket} = this.state;     
-    let buttonProgressText = 'ดำเนินการ'
-    let buttonProgressClassName = success? 'movie-cashier__confirm success':'movie-cashier__confirm'
-    buttonProgressClassName += postingTicket? ' posting' : ''
-    if (postingTicket) {
-      buttonProgressText = 'กรุณารอสักครู่'
-    } 
+    const {isLoading, error, dataToTicket} = this.state;    
     if (error) {
       return <p>{error.message}</p>;
     }
     if (isLoading) { 
       return <img src={loading} className="loading"/>
     } 
-
-    return (
-      <div className="movie-cashier__container">
-        <div className="movie-cashier__movie-info">
-          <img className="movie-cashier__poster" src={this.state.BookingPoster}/>
-          <div className="movie-cashier__wrapper">
-            <h2 className="movie-cashier__title">{this.state.BookingMovie}</h2>
-            <h3 className="movie-cashier__subtitle">{this.state.BookingMovieTH}</h3>
-            <span className="movie-cashier__genre">{this.state.BookingGenre}<br/> {this.state.BookingDuration}</span>
-          </div>
-        </div>
-        <div className="movie-cashier__cine-info">
-            <span className="movie-cashier__cine-info--title">{this.state.BookingCinema}</span>
-            <span className="movie-cashier__cine-info--label">สาขา</span>
-            <span className="movie-cashier__cine-info--map">ดูแผนที่</span>
-        </div>
-        <div className="movie-cashier__date-info">
-          <div className="movie-cashier__date-info--wrapper">
-            <span className="movie-cashier__date-info--label">วันที่</span>
-            <span className="movie-cashier__date-info--title">{this.state.BookingDate}</span>
-          </div>
-          <div className="movie-cashier__date-info--wrapper">
-            <span className="movie-cashier__date-info--label">เวลา</span>
-            <span className="movie-cashier__date-info--title">{this.state.BookingTime}</span>
-          </div>
-        </div>
-        <div className="movie-cashier__seat-info">
-          <div className="movie-cashier__seat-info--wrapper">
-            <span className="movie-cashier__seat-info--label">โรงภาพยนตร์</span>
-            <span className="movie-cashier__seat-info--title">{this.state.BookingScreenName}</span>
-          </div>
-          <div className="movie-cashier__seat-info--wrapper">
-            <span className="movie-cashier__seat-info--label">หมายเลขที่นั่ง</span>
-            <span className="movie-cashier__seat-info--title">{this.state.BookingSeat}</span>
-          </div>
-        </div>
-        <div className="movie-cashier__seat-info">
-          <div className="movie-cashier__seat-info--wrapper">
-            <span className="movie-cashier__seat-info--title">เสียง {this.state.BookingAttributesNames}</span>
-          </div>
-          <div className="movie-cashier__seat-info--wrapper">
-            <span className="movie-cashier__seat-info--title">ราคา {this.state.BookingPriceDisplay} บาท</span>
-          </div>
-        </div>
-        <div className={success? 'qrContainer success':'qrContainer'}>
-          <div className="qrContainer__qrcode">
-            <QRCode size={150} level={"H"} value={this.state.VistaBookingNumber} />
-          </div>
-          <b className="qrContainer__ref">{this.state.VistaBookingId}</b>
-        </div>
-        <div className={buttonProgressClassName} onClick={this.submitPayment.bind(this)}>{buttonProgressText}</div>
-        <div className={success? 'movie-cashier__confirm':'movie-cashier__confirm success'}>เสร็จสิ้น</div>
-      </div>
-    );
-  }
-}
-
-class Cashier extends PureComponent {
-  render() {
     return (
       <Layout title="Cashier Page">
-        <header className="cashier-header">ยืนยันที่นั่ง</header>
-        <CinemaMovieInfo/>
+        <Ticket dataTicket={dataToTicket} submitPayment={this.submitPayment.bind(this)}></Ticket>
       </Layout>
     );
   }
