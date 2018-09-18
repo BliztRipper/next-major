f200839accff7da15e46b9d2b90c0c034a73ccd9
Nattapong Srivorakul <>
2018-09-18 14:05:46 +0700

    remove conflict

--- components/MovieInfoComp.jsx
+++ components/MovieInfoComp.jsx
@@ -12,21 +12,6 @@ class MovieInfoComp extends PureComponent {
       return img
     }
     render() {
-<<<<<<< HEAD
-      if (this.props.item) {
-        return (
-          <Fragment>
-            <div className="movie-card__container">
-              <img className="movie-card__poster" src={this.props.item.poster_ori} />
-              <div className="movie-card__wrapper">
-                <h2 className="movie-card__title">{this.props.item.title_en}</h2>
-                <h3 className="movie-card__subtitle">{this.props.item.title_th}</h3>
-                <span className="movie-card__genre">{this.props.item.genre} | {this.props.item.duration} นาที</span>
-                <Link prefetch href='/MovieInfo'>
-                  <a className="movie-card__more-detail">ดูรายละเอียดเพิ่ม</a>
-                </Link>
-              </div>
-=======
       const poster = {
         width:'100%',
         left: 0,
@@ -83,14 +68,9 @@ class MovieInfoComp extends PureComponent {
               <Link prefetch href='/MovieInfo'>
                 <a className="movie-card__more-detail" style={detail}>รายละเอียด</a>
               </Link>
->>>>>>> feature/fix_video
             </div>
-          </Fragment>
-        )
-      }
-
-      return (
-        <Fragment></Fragment>
+          </div>
+        </Fragment>
       )
     }
   }
