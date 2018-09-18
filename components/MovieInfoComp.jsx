6ec1ef7f66a894439be874b6ee1c8e8c5862083b
Blizt <nonthegenocide@me.com>
2018-09-18 11:48:17 +0700

    add movie info background

--- components/MovieInfoComp.jsx
+++ components/MovieInfoComp.jsx
@@ -3,17 +3,70 @@ import Link from 'next/link'
 import '../styles/style.scss'
 
 class MovieInfoComp extends PureComponent {
+    movieImage(img){
+      if(img === ""){
+        img = '../static/empty2.png'
+      } else {
+        img = img
+      }
+      return img
+    }
     render() {
+      const poster = {
+        width:'100%',
+        left: 0,
+        right: 0,
+        zIndex: -1,
+        position: 'absolute',
+        filter: 'brightness(0.4)',
+        height: '30vh',
+      }
+      const wrapper ={
+        marginLeft:0,
+      }
+      const detail ={
+        backgroundColor: '#989fae',
+        borderRadius: '4px',
+        color: '#fff',
+        display: 'block',
+        fontSize: '0.875rem',
+        marginTop: '1rem',
+        padding: '6px 8px',
+        textDecoration: 'none',
+        whiteSpace: 'nowrap',
+        width: '100px',
+        marginLeft: 'auto',
+        marginRight: 'auto'
+      }
+      const title = {
+        fontSize: '1.125rem',
+        fontWeight: 700,
+        marginBottom: '1rem',
+        color:'#fff',
+        textAlign: 'center',
+      }
+      const subtitle = {
+        fontSize: '.875rem',
+        fontWeight: 700,
+        margin: '0',
+        color:'#fff',
+        textAlign: 'center'
+      }
+      const genre = {
+        fontSize: '.75rem',
+        textAlign:'center',
+        color:'#fff'
+      }
       return (
         <Fragment>
+          <img className="movie-card__poster" src={this.movieImage(this.props.item.movie_image)} style={poster}/>
           <div className="movie-card__container">
-            <img className="movie-card__poster" src={this.props.item.poster_ori} />
-            <div className="movie-card__wrapper">
-              <h2 className="movie-card__title">{this.props.item.title_en}</h2>
-              <h3 className="movie-card__subtitle">{this.props.item.title_th}</h3>
-              <span className="movie-card__genre">{this.props.item.genre} | {this.props.item.duration} นาที</span>
+            <div className="movie-card__wrapper" style={wrapper}>
+              <h2 className="movie-card__title" style={title}>{this.props.item.title_en}</h2>
+              <h3 className="movie-card__subtitle" style={subtitle}>{this.props.item.title_th}</h3>
+              <span className="movie-card__genre" style={genre}>{this.props.item.genre} | {this.props.item.duration} นาที</span>
               <Link prefetch href='/MovieInfo'>
-                <a className="movie-card__more-detail">ดูรายละเอียดเพิ่ม</a>
+                <a className="movie-card__more-detail" style={detail}>รายละเอียด</a>
               </Link>
             </div>
           </div>
