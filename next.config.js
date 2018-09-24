2637e5303b53f4b44b439bd5f054de7a3c1681b0
Blizt <nonthegenocide@me.com>
2018-09-24 17:47:58 +0700

    fix analyzer require

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 const withSass = require('@zeit/next-sass')
 const withImages = require('next-images')
 const withFonts = require('next-fonts')
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 const { ANALYZE } = process.env
 
 module.exports = withFonts(withSass(withImages({
   webpack: function (config, { isServer }) {
     if (ANALYZE) {
-      const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
       config.plugins.push(new BundleAnalyzerPlugin({
         analyzerMode: 'server',
         analyzerPort: isServer ? 8888 : 8889,
