67de4db78c9ff44a03defce798f241a521752235
Blizt <nonthegenocide@me.com>
2018-10-04 11:19:36 +0700

    add polyfill

--- next.config.js
+++ next.config.js
@@ -7,7 +7,47 @@ const withOffline = require('next-offline')
 const withPlugins = require('next-compose-plugins');
 const optimizedImages = require('next-optimized-images');
 
-module.exports = withOffline(withImages(withSass(withFonts({
+
+module.exports = withPlugins([
+  [optimizedImages, {
+    inlineImageLimit: 8192,
+    imagesFolder: 'static',
+    imagesName: '[name]-[hash].[ext]',
+    optimizeImagesInDev: false,
+    mozjpeg: {
+        quality: 80,
+    },
+    optipng: {
+        optimizationLevel: 3,
+    },
+    pngquant: false,
+    gifsicle: {
+        interlaced: true,
+        optimizationLevel: 3,
+    },
+    svgo: {
+
+    },
+    webp: {
+        preset: 'default',
+        quality: 75,
+    },
+  }],
+  {
+    webpack: function (cfg) {
+      const originalEntry = cfg.entry
+      cfg.entry = async () => {
+        const entries = await originalEntry()
+        if (entries['main.js'] && !entries['main.js'].includes('./client/polyfills.js')) {
+          entries['main.js'].unshift('./client/polyfills.js')
+        }
+        return entries
+      }
+
+      return cfg
+    }
+  },
+  withOffline(withFonts(withImages(withSass({
     webpack: function (config, { isServer }) {
       if (ANALYZE) {
         config.plugins.push(new BundleAnalyzerPlugin({
@@ -19,42 +59,4 @@ module.exports = withOffline(withImages(withSass(withFonts({
       return config
     }
   }))))
-
-// module.exports = withPlugins([
-//   [optimizedImages, {
-//     inlineImageLimit: 8192,
-//     imagesFolder: 'static',
-//     imagesName: '[name]-[hash].[ext]',
-//     optimizeImagesInDev: false,
-//     mozjpeg: {
-//         quality: 80,
-//     },
-//     optipng: {
-//         optimizationLevel: 3,
-//     },
-//     pngquant: false,
-//     gifsicle: {
-//         interlaced: true,
-//         optimizationLevel: 3,
-//     },
-//     svgo: {
-
-//     },
-//     webp: {
-//         preset: 'default',
-//         quality: 75,
-//     },
-//   }],
-//   withOffline(withImages(withSass(withFonts({
-//     webpack: function (config, { isServer }) {
-//       if (ANALYZE) {
-//         config.plugins.push(new BundleAnalyzerPlugin({
-//           analyzerMode: 'server',
-//           analyzerPort: isServer ? 8888 : 8889,
-//           openAnalyzer: true
-//         }))
-//       }
-//       return config
-//     }
-//   }))))
-// ]);
+]);
