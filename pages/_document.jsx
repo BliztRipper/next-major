7efcaed0d1a89ac5d83ee609e7089304b8b3915e
Blizt <nonthegenocide@me.com>
2018-10-29 17:56:51 +0700

    set inner HTML tag script

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,12 +5,15 @@ export default class MyDocument extends Document {
     return (
       <html lang="en">
         <Head>
-        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-PHNHLKK');</script>
-        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PHNHLKK"height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <script dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PHNHLKK');`,
+        }}>
+        </script>
+        <noscript dangerouslySetInnerHTML={{__html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PHNHLKK"height="0" width="0" style="display:none;visibility:hidden"></iframe>`}}></noscript>
         </Head>
         <body>
           <Main />
