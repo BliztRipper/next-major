715afc88bc9b4ec53b39f38b61304f672cd66790
korakot.sup <korakot.sup@ascendcorp.com>
2019-01-14 17:15:11 +0700

    async/await done.

--- tools/checkingSeatsInGroup.js
+++ tools/checkingSeatsInGroup.js
@@ -1,78 +1,79 @@
 const axios = require('axios')
 const apiUrlBase = 'https://api-cinema.truemoney.net'
 let branchIncreasement = 0
-let previousTheatreScreenNumber = ''
+let totalSessionIds = 0
 let previousSessionId = ''
 
-const getSeatsIngroup = (cinema, theatre, moive) => {
+const mapTicketPricesWithSeatPlans = (ticketPrices, SeatPlan, theatre, movie, sessionId) => {
+  console.log(`>>> Theatre Name: ${theatre.ScreenName} || Theatre Number: ${theatre.ScreenNumber} || SessionId: ${sessionId} || Showtimes: ${movie.Showtimes}`);
+  let instantTicketAreas = ticketPrices.Tickets
+  let instantSeatAreas = SeatPlan.SeatLayoutData.Areas
+  instantSeatAreas.forEach((seatArea, seatAreaIndex, seatAreaArray) => {
+    let lastSeatArea = seatAreaIndex + 1 === seatAreaArray.length
+     instantTicketAreas.forEach((ticketArea, ticketAreaIndex, ticketAreaArray) => {
+      let lastTicketArea = ticketAreaIndex + 1 === ticketAreaArray.length
+      if (seatArea.AreaCategoryCode === ticketArea.AreaCategoryCode && ticketArea.IsPackageTicket) {
+         seatArea.Rows.forEach((seatRow, seatRowIndex, seatRowArray) => {
+          let lastRow = seatRowIndex + 1 === seatRowArray.length
+          let seatsInRows = []
+          if (seatRow.PhysicalName && seatRow.Seats && seatRow.Seats.length > 0) {
+             seatRow.Seats.forEach((seat, seatIndex, seatArray) => {
+              if (!seat.SeatsInGroup) {
+                seatsInRows.push(seat.Id)
+              }
+              if (seatIndex + 1 === seatArray.length && seatsInRows.length > 0) {
+                if (sessionId != previousSessionId) {
+                  totalSessionIds += 1
+                  previousSessionId = sessionId
+                  console.log('=====');
+                  console.log('=====');
+                  console.log(`Theatre Name: ${theatre.ScreenName} || Theatre Number: ${theatre.ScreenNumber}`);
+                  console.log(`SessionId: ${sessionId} || Showtimes: ${movie.Showtimes}`);
+                  console.log(`AreaCategoryCode: ${ticketArea.AreaCategoryCode}`);
+                  console.log(`SeatsInGroup : ${seat.SeatsInGroup}`);
+                  console.log('...');
+                }
+                console.log('...');
+                console.log(`row: ${seatRow.PhysicalName}`);
+                console.log(`Seat IDs : ${seatsInRows.join()}`);
+                console.log('...');
+              }
+            });
+          }
+        });
+      }
+    });
+  });
+}
+
+const getSeatsIngroup = async (cinema, theatre, movie) => {
   let cinemaId = cinema.ID
-  let sessionId = moive.SessionIds
+  let sessionId = movie.SessionIds
   let apiUrls = {
     ticketPrices : `${apiUrlBase}/TicketPrices/${cinemaId}/${sessionId}`,
     seatPlan : `${apiUrlBase}/SeatPlan/${cinemaId}/${sessionId}`
   }
 
-  let instantData = {
-    ticketPrices: '',
-    seatPlan: ''
-  }
-
-  const mapTicketPricesWithSeatPlans = (ticketPrices, SeatPlan) => {
-
-    let instantTicketAreas = ticketPrices.Tickets
-    let instantSeatAreas = SeatPlan.SeatLayoutData.Areas
-    instantSeatAreas.forEach(seatArea => {
-      instantTicketAreas.forEach((ticketArea, ticketAreaIndex) => {
-        if (seatArea.AreaCategoryCode === ticketArea.AreaCategoryCode && ticketArea.IsPackageTicket) {
-          seatArea.Rows.forEach((seatRow) => {
-            let seatsInRows = []
-            if (seatRow.PhysicalName && seatRow.Seats && seatRow.Seats.length > 0) {
-              seatRow.Seats.forEach((seat, seatIndex, seatArray) => {
-                if (!seat.SeatsInGroup) {
-                  seatsInRows.push(seat.Id)
-                }
-                if (seatIndex + 1 === seatArray.length && seatsInRows.length > 0) {
-                  if (sessionId != previousSessionId) {
-                    previousSessionId = sessionId
-                    console.log('=====');
-                    console.log('=====');
-                    console.log(`Theatre Name: ${theatre.ScreenName} || Theatre Number: ${theatre.ScreenNumber}`);
-                    console.log(`SessionId: ${sessionId} || Showtimes: ${moive.Showtimes}`);
-                    console.log(`AreaCategoryCode: ${ticketArea.AreaCategoryCode}`);
-                    console.log(`SeatsInGroup : ${seat.SeatsInGroup}`);
-                    console.log('...');
-                  }
-                  console.log('...');
-                  console.log(`row: ${seatRow.PhysicalName}`);
-                  console.log(`Seat IDs : ${seatsInRows.join()}`);
-                  console.log('...');
-                }
-              });
-            }
-          });
-        }
-      });
-    });
-  }
-
+  let apiPromises = []
   Object.keys(apiUrls).forEach(key => {
-    axios({
-      method: 'get',
-      url: apiUrls[key]
-    })
-    .then((res) => {
-      let instantTicketsOrSeatData = res.data
-      if (instantTicketsOrSeatData.status_code === 0) {
-        instantData[key] = instantTicketsOrSeatData.data
-        if (instantData.ticketPrices && instantData.seatPlan) {
-          mapTicketPricesWithSeatPlans(instantData.ticketPrices, instantData.seatPlan)
-        }
-      }
-    })
-    // .catch((error) => {
-      // console.error(error.config.url, 'error')
-    // })
+    apiPromises.push(axios.get(apiUrls[key]))
   });
+
+  return await axios.all(apiPromises)
+  .then(axios.spread(async function (ticketPricesResponse, seatPlanResponse) {
+    let instantTicketPricesData = ticketPricesResponse.data
+    let instantSeatPlanResponseData = seatPlanResponse.data
+    if (instantTicketPricesData.status_code === 0 && instantSeatPlanResponseData.status_code === 0 ) {
+      return await mapTicketPricesWithSeatPlans(instantTicketPricesData.data, instantSeatPlanResponseData.data,theatre, movie, sessionId)
+    }
+  }))
+  .catch(function (error) {
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+
 }
 
 const getScheduleInBranchCinema = (cinema) => {
@@ -86,19 +87,31 @@ const getScheduleInBranchCinema = (cinema) => {
     url: `${apiUrlBase}/Schedule`,
     data: dataToGetSchedule
   })
-  .then((res) => {
+  .then(async (res) => {
     let instantSchedulesDataInCinemas = res.data
     if (instantSchedulesDataInCinemas.status_code === 0) {
-      instantSchedulesDataInCinemas.data[0].Theaters.forEach((theatre, theatreIndex) => {
-        theatre.MovieInTheaters.forEach((moive, moiveIndex) => {
-          getSeatsIngroup(cinema, theatre, moive)
-        });
-      });
+      let theatres = instantSchedulesDataInCinemas.data[0].Theaters
+      for (let theatresIndex = 0; theatresIndex < theatres.length; theatresIndex++) {
+        let lastTheatre = theatresIndex + 1 === theatres.length
+        let theatre = theatres[theatresIndex];
+        for (let movieIndex = 0; movieIndex < theatre.MovieInTheaters.length; movieIndex++) {
+          let lastMovie = movieIndex + 1 === theatre.MovieInTheaters.length
+          let movie = theatre.MovieInTheaters[movieIndex];
+          await getSeatsIngroup(cinema, theatre, movie)
+        }
+      }
+      console.log('Start next getBranches');
+      getBranches()
+
     }
+
   })
-  // .catch((error) => {
-    // console.error(error.config.url, 'error')
-  // })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
 }
 const getBranches = () => {
   axios({
@@ -113,29 +126,39 @@ const getBranches = () => {
         console.log('======================');
         console.log(`========== BEGIN TASK ==========`);
       }
-      console.log('===');
-      console.log('===');
-      console.log('===');
-      console.log('===');
-      console.log('===');
-      console.log('===');
-      console.log(`CinemaId: ${instantBranchesData[branchIncreasement].ID} || Cinema Name: ${instantBranchesData[branchIncreasement].Name ? instantBranchesData[branchIncreasement].Name : instantBranchesData[branchIncreasement].NameAlt}`);
-      console.log(`Total Branch : ${branchIncreasement + 1}/${instantBranchesData.length}`);
-      console.log('===');
-      console.log('===');
       if (branchIncreasement < instantBranchesData.length) {
+        console.log('===');
+        console.log('===');
+        console.log('===');
+        console.log('===');
+        console.log('===');
+        console.log('===');
+        console.log(`CinemaId: ${instantBranchesData[branchIncreasement].ID} || Cinema Name: ${instantBranchesData[branchIncreasement].Name ? instantBranchesData[branchIncreasement].Name : instantBranchesData[branchIncreasement].NameAlt}`);
+        console.log(`Total Branch : ${branchIncreasement + 1}/${instantBranchesData.length}`);
+        console.log('===');
+        console.log('===');
         getScheduleInBranchCinema(instantBranchesData[branchIncreasement])
-        branchIncreasement += 1
       }
+      branchIncreasement += 1
       if (branchIncreasement === instantBranchesData.length) {
+        console.log(`Total SessionIds : ${totalSessionIds}`);
         console.log(`========== END TASK ==========`);
         console.log('======================');
       }
     }
   })
-  // .catch((error) => {
-    // console.error(error.config.url, 'error')
-  // })
+  .catch(function (error) {
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
 }
 
 getBranches()
+
+
+// process.on('exit', function(code) {
+  // getBranches()
+  // return console.log(`About to exit with code ${code}`);
+// });
